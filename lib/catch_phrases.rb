c7999c35ae1d840b7c52002963301c29f1ec3479
Jazz Bozner <jazzbozner@gmail.com>
2020-01-06 23:13:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,20 +1,3 @@
-def mario
-  phrase = "It's-a me, Mario!"
-  puts phrase
-end
 
-def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
-end
-
-def link 
-  warning = "It's Dangerous To Go Alone! Take This."
-  puts warning
-end
-
-def any_phrase(phrase)
-  puts phrase
-end
 
   
